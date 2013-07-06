228838bdaeb976b3b612da2a8ee89dbb45c2eaf3
Benjamin Brink <tekbasse@yahoo.com>
2013-07-06 18:12:29 +0000

    starting hf utility api

--- tcl/hosting-farm-procs.tcl
+++ tcl/hosting-farm-procs.tcl
@@ -4,15 +4,54 @@ ad_library {
     @creation-date 5 June 2013
 }
 
-ad_proc -public hf_asset_ids_for_user { 
-    user_id
+# following defined in permissions-procs.tcl
+# hf_customer_ids_for_user
+# hf_active_asset_ids_for_customer 
+
+ad_proc -private hf_asset_ids_for_user { 
     {instance_id ""}
+    {user_id ""}
 } {
-    Returns asset_ids for user_id
+    Returns asset_ids available to user_id as list of lists (each list represents ids by  one customer)
 } {
+    if { $instance_id eq "" } {
+        # set instance_id package_id
+        set instance_id [ad_conn package_id]
+    }
+    if { $user_id eq "" } {
+        set user_id [ad_conn user_id]
+    }
+    set customer_ids_list [hf_customer_ids_for_user $user_id]
+    # get asset_ids assigned to customer_ids
+    set asset_ids_list [list ]
+    foreach customer_id $customer_ids_list {
+        set assets_list [hf_asset_ids_for_customer_id $instance_id $customer_id]
+        if { [llength $assets_list > 0 ] } {
+            lappend asset_ids_list $assets_list
+        }
+    }
+    return $asset_ids_list
+}
+
+ad_proc -private hf_asset_ids_for_customer_id {
+    {instance_id ""}
+    customer_id
+} {
+    Returns asset_ids for customer_id as a list
+} {
+    if { $instance_id eq "" } {
+        # set instance_id package_id
+        set instance_id [ad_conn package_id]
+    }
+    ## make sure that user_id has permission to access customer_id info
+    # check permissions
+    set user_id [ad_conn user_id]
+    set read_p [hf_permission_p $instance_id $user_id $customer_id "customer_id-${customer_id}" read]
+
+
 
 }
-# hf_asset_ids_for_customer_id instance_id customer_id
+
 
 # hf_asset_create_from_asset_template instance_id asset_id args
 # hf_asset_create_from_asset_label instance_id asset_label args
