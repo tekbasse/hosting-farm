50b86fbec8f8ddd312ee2f61474453ce0235336b
Benjamin Brink <tekbasse@yahoo.com>
2013-12-12 11:31:21 -0800

    coded basic logic for summary report

--- lib/resource-status-summary-1.tcl
+++ lib/resource-status-summary-1.tcl
@@ -1,8 +1,12 @@
 # hosting-farm/lib/resource-status-summary-1.tcl
 # requires:
+# interval_remaining_ts timestamp in seconds
 # 
 # optional: 
 # list_limit
+# # following two options might be alternatives to interval_remaining_ts
+# now_ts in timestamp seconds
+# interval_length_ts in timestamp seconds
          
 # This is configured as a demo.
 # A release version would require user_id, customer_ids_list..
@@ -12,6 +16,9 @@ if { ![info exists compact_p ] } {
     set compact_p 0
 }
 
+if { ![info exists now_ts] } {
+    set now_ts [clock seconds]
+}
 # use percent measurements and faces for compact style
 
 # EOT = end of term
@@ -74,20 +81,17 @@ foreach q_list $quota_lists {
 
 # asset db
 # attribute = meter_type = metric
-# as_label as_name as_type metric lastest_sample unit
+# as_label as_name as_type 
+set asset_db_lists [list ]
 set as_count [expr { int( rand() * 50 ) + 1 } ]
 for { set i 1} {$i < $as_count} {incr i} {
     set as_label [lindex $random_names_list $i]
     set as_name $as_label
     set as_type [lindex $as_type_list [expr { int( rand() * $as_type_count + .99 ) } ]]
-    foreach as_at $asr_arr($as_type) {
-        set as_list [list ]############
-                     lappend asset_db_lists $as_list
-                 }
+    # as_label as_name as_type
+    set as_list [list $as_label $as_name $as_type]
+    lappend asset_db_lists $as_list
 }
-# report db built from hf_monitor_config_n_control, monitor_log, hf_monitor_status
-# reportdb asset_label as_type as_attribute monitor_label portions_count health report_id
-# for quota monitoring, portions_count is count per last two weeks.
 
 
 # health score is 0 to 16, interprets statistics
@@ -108,8 +112,79 @@ for { set i 1} {$i < $as_count} {incr i} {
 set theme "rock"
 set icon_name_list [list "inactive" "active" "alert" "amusement" "joy" "joy" "joy" "joy" "joy" "joy" "joy" "joy" "joy" "laugh" "anxious" "surprise"]
 
+
+
+# for the demo,  manually build a fake list instead of calling a procedure
 # as_label as_name as_type metric latest_sample unit percent_quota projected_eop score score_message
+foreach asset_list $asset_db_lists {
+    # asset_list is a list of asset attributes: label, name, type
+    set as_type [lindex $asset_list 2]
+    # start rolling dice..
+    set active_p [expr { int( rand() * 16 ) > 1 } ] 
 
+    set as_reports_list $asset_list
 
+    if { $active_p } {
+        # sometimes an account is new, so not enough info exists
+        # role dice..
+        set history_exists_p [expr { int( rand() * 16.99 ) } ]
 
+        # metric1 metric2 metric..
+        foreach as_at $asr_arr($as_type) {
+            # quota_arr(as_type,metric) = quota amount
+            set quota $quota(${as_type},${as_at})
+            # 16 health scores, lets randomly try to get all cases for demo and testing
+            # only 1/8 are stressfull , 1/8 = 0.125
+            set sample [expr { int( rand() * $quota * 1.125 ) } ]
+            set unit "B"
+            set pct_quota [expr { 100. * $sample / ( $quota * 1.) } ]
+            if { $history_exists_p } {
+                set weeks_rate [expr { int( rand() * $sample / 3. ) } ]  
+                # convert rate to interval_length
+                # 1 week = 604800 seconds
+                # interval_rate = units_per_week / secs_per_week * secs_per_interval_remaining
+                set interval_rate [expr { $weeks_rate * $interval_remaining_ts / 604800. } ]
+                set projected_eop [expr { $sample + $interval_rate } ]
+                set $projected_eop_html $projected_eop
+            } else {
+                # not enough history to calculate
+                set $projected_eop 0
+                set $projected_eop_html "#accounts-ledger.N_A#"
+            }
+            
+            # calc health score value
+            # initial health is based on background performance..
+            set health_score [expr { int( rand() * 11 ) + 1 } ]
+            set hs_message "#accounts-ledger.${health_score}#"
+            if { $projected_eop > [expr { $quota * 0.9 } ] || $pct_quota > 0.8 } {
+                set health_score 13
+                set hs_message "Near quota limit."
+            }
+            if { $projected_eop > $quota } {
+                set health_score 14
+                set hs_message "May be over quota before end of term."
+            }
+            if { $pct_quota > 1 } {
+                set health_score 15
+                set hs_message "Over quota."
+            }
+            # $as_at is metric
+            lappend as_reports_list $as_at
+            lappend as_reports_list $sample $unit $pct_quota $projected_eop $health_score $hs_message
+            # as_label as_name as_type metric latest_sample unit percent_quota projected_eop score score_message
+            lappend asset_db_lists $as_report_list
+        }
+    } else {
+        # asset not active
+        set health_score 0
+        set hs_message ""
+        lappend as_reports_list "#accounts-ledger.N_A#" "" "" "" "" "" ""
+        lappend asset_db_lists $as_report_list
+    }
+
+}
+
+# report db built from hf_monitor_config_n_control, monitor_log, hf_monitor_status
+# reportdb asset_label as_type as_attribute monitor_label portions_count health report_id
+# for quota monitoring, portions_count is count per last two weeks.
 
