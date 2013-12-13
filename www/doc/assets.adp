c4f3bdd03a0664f6506f576bf1570727afa5fd89
Benjamin Brink <tekbasse@yahoo.com>
2013-12-12 23:32:43 -0800

    adding another test demo case.

---
+++ www/doc/assets.adp
@@ -0,0 +1,9 @@
+<master src="/packages/hosting-farm/www/doc/responsive-master3">
+  <property name="title">@title;noquote@</property>
+  <property name="context">@context;noquote@</property>
+
+    <div>Welcome @user_name@</div> 
+    <div>Resource summary:<br>
+      <include src="/packages/hosting-farm/lib/resource-status-summary-1" interval_remaining="@interval_remaining;noquote@" columns="4"> 
+    </div> 
+
---
+++ www/doc/assets.tcl
@@ -0,0 +1,32 @@
+# defaults
+set title "menu"
+set context [list $title]
+
+# User information and top level navigation links
+#
+set user_id [ad_conn user_id]
+set package_id [ad_conn package_id]
+set untrusted_user_id [ad_conn untrusted_user_id]
+set sw_admin_p 0
+
+if { $untrusted_user_id == 0 } {
+    # The browser does NOT claim to represent a user that we know about
+    set login_url [ad_get_login_url -return]
+    set user_name ""
+} else {
+    # The browser claims to represent a user that we know about
+    set user_name [person::name -person_id $untrusted_user_id]
+    set pvt_home_url [ad_pvt_home]
+    set pvt_home_name [_ acs-subsite.Your_Account]
+    set logout_url [ad_get_logout_url]
+    # Site-wide admin link
+    set admin_url {}
+    set sw_admin_p [acs_user::site_wide_admin_p -user_id $untrusted_user_id]
+
+}
+
+# get status info
+#  see as.adp as.tcl for db scenario example
+
+set interval_remaining [expr { int( rand() * 30 ) } ]
+
