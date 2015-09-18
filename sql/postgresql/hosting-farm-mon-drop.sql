44109d9e4bb4052220db6dfd49945e0b5b14164d
Benjamin Brink <tekbasse@yahoo.com>
2015-09-18 15:11:06 -0400

    revised drop to fit creations.

--- sql/postgresql/hosting-farm-mon-drop.sql
+++ sql/postgresql/hosting-farm-mon-drop.sql
@@ -3,30 +3,26 @@
 -- @author
 -- @cvs-id
 --
---DROP index hf_beat_args_stack_id;
 
---DROP TABLE hf_beat_args;
-
-DROP index hf_beat_stack_started_time_key;
 DROP index hf_beat_stack_priority_key;
 DROP index hf_beat_stack_id_key;
-
 DROP TABLE hf_beat_stack;
 
-DROP TABLE hf_beat_stack_active;
+DROP TABLE hf_beat_stack_bus;
 
-DROP index hf_beat_log_id_idx;
-DROP index hf_beat_log_instance_id_idx;
-DROP index hf_beat_log_user_id_idx;
-DROP index hf_beat_log_table_tid_idx;
-DROP index hf_beat_log_trashed_p_idx;
-DROP index hf_beat_log_viewed_id_idx;
-DROP index hf_beat_log_viewed_instance_id_idx;
-DROP index hf_beat_log_viewed_user_id_idx;
-DROP index hf_beat_log_viewed_table_tid_idx;
+drop index hf_beat_log_viewed_asset_id_idx;
+drop index hf_beat_log_viewed_user_id_idx;
+drop index hf_beat_log_viewed_instance_id_idx;
+drop index hf_beat_log_viewed_id_idx;
 
-DROP TABLE hf_beat_log;
 DROP TABLE hf_beat_log_viewed;
 
-DROP SEQUENCE hf_beat_id_seq;
+drop index hf_beat_log_trashed_p_idx;
+drop index hf_beat_log_asset_id_idx;
+drop index hf_beat_log_user_id_idx;
+drop index hf_beat_log_instance_id_idx;
+drop index hf_beat_log_id_idx;
 
+DROP TABLE hf_beat_log;
+
+DROP SEQUENCE hf_beat_id_seq;
