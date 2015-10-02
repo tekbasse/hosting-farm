4bb56c120510a3e52986b406680d663f85a4892c
Benjamin Brink <tekbasse@yahoo.com>
2015-10-01 17:57:41 -0700

    more code..

--- tcl/hosting-farm-monitor-procs.tcl
+++ tcl/hosting-farm-monitor-procs.tcl
@@ -51,7 +51,7 @@ ad_proc -private hf::monitor::check {
     upvar 1 debug_p debug_p 
     upvar 1 priority_threashold priority_threashold
     upvar 1 cycle_time cycle_time
- 
+    set active_id ""
     if { ![db_0or1row hf_beat_stack_bus_ck "select active_id, debug_p,priority_threashold,cycle_time from hf_beat_stack_bus limit 1"] } {
 
         #CREATE TABLE hf_beat_stack_bus (
@@ -98,143 +98,148 @@ ad_proc -private hf::monitor::do {
 
     # First, check if a monitor process is running and get status of debug_p
     hf::monitor::check
-    set success_p 0
+    if { $active_id eq "" } {
+        set success_p 0
 
-#       -- stack is prioritized by
-#       -- time must be > last time + interval_s + last_completed_time 
-#       -- priority
-#       -- relative priority: priority - (now - last_completed_time )/ interval_s - last_completed_time
-#       -- relative priority kicks in after threashold priority procs have been exhausted for the interval
-#       -- trigger_s is  ( last_started_clock_s + last_process_s - interval_s ) 
-    set clock_sec [clock seconds]
-    set batch_lists [db_list_of_lists hf_beat_stack_read_adm_p0_s { select id,proc_name,user_id,instance_id, priority, order_clock_s, last_started_clock_s,last_completed_clock_s,last_process_s,interval_s,(priority - (:clock_sec - last_completed_clock_s) /greatest('1',interval_s ) + last_process_s) as dynamic_priority , trigger_s from hf_beat_stack where trigger_s < :clock_sec, order by priority asc, dynamic_priority asc } ]
+        #       -- stack is prioritized by
+        #       -- time must be > last time + interval_s + last_completed_time 
+        #       -- priority
+        #       -- relative priority: priority - (now - last_completed_time )/ interval_s - last_completed_time
+        #       -- relative priority kicks in after threashold priority procs have been exhausted for the interval
+        #       -- trigger_s is  ( last_started_clock_s + last_process_s - interval_s ) 
+        set clock_sec [clock seconds]
+        set batch_lists [db_list_of_lists hf_beat_stack_read_adm_p0_s { select id,proc_name,user_id,instance_id, priority, order_clock_s, last_started_clock_s,last_completed_clock_s,last_process_s,interval_s,(priority - (:clock_sec - last_completed_clock_s) /greatest('1',interval_s ) + last_process_s) as dynamic_priority , trigger_s from hf_beat_stack where trigger_s < :clock_sec, order by priority asc, dynamic_priority asc } ]
 
-#CREATE TABLE hf_beat_stack (
-#       id integer primary key,
-#       -- Assumes procedure is called repeatedly
-#       -- Since procedure is repeated, cannot
-#       -- use empty completed_time to infer active status
-#       -- instead, see hf_beat_stack_bus.active_id
-#
-#       -- stack is prioritized by
-#       -- time must be > last time + interval_s + last_process_time_s
-#       -- priority
-#       -- relative priority: priority - (now - last_completed_time )/ interval_s + last_process_s
-#       -- relative priority kicks in after threashold priority procs have been exhausted for the interval
-#       proc_name varchar(40),
-#       proc_args text,
-#       proc_out text,
-#       user_id integer,
-#       instance_id integer,
-#       priority integer,
-#       -- when first requested in  machine clock seconds
-#       order_clock_s integer,
-#       -- last time proc was started in machine clock seconds
-#       last_started_clock_s integer,
-#       -- last time proc completed in machine clock seconds
-#       last_completed_clock_s integer,
-#       -- response_time in seconds; should be about same as last_completed_time - last_started_time
-#       last_process_s integer,
-#       -- requested interval between calls
-#       -- this value is extracted from hf_monitor_config_n_control
-#       interval_s integer,
-#       trigger_s integer,
-#       order_time timestamptz,
-#       last_started_time timestamptz,
-#       last_completed_time timestamptz,
-#       call_counter integer
-#);
+        #CREATE TABLE hf_beat_stack (
+        #       id integer primary key,
+        #       -- Assumes procedure is called repeatedly
+        #       -- Since procedure is repeated, cannot
+        #       -- use empty completed_time to infer active status
+        #       -- instead, see hf_beat_stack_bus.active_id
+        #
+        #       -- stack is prioritized by
+        #       -- time must be > last time + interval_s + last_process_time_s
+        #       -- priority
+        #       -- relative priority: priority - (now - last_completed_time )/ interval_s + last_process_s
+        #       -- relative priority kicks in after threashold priority procs have been exhausted for the interval
+        #       proc_name varchar(40),
+        #       proc_args text,
+        #       proc_out text,
+        #       user_id integer,
+        #       instance_id integer,
+        #       priority integer,
+        #       -- when first requested in  machine clock seconds
+        #       order_clock_s integer,
+        #       -- last time proc was started in machine clock seconds
+        #       last_started_clock_s integer,
+        #       -- last time proc completed in machine clock seconds
+        #       last_completed_clock_s integer,
+        #       -- response_time in seconds; should be about same as last_completed_time - last_started_time
+        #       last_process_s integer,
+        #       -- requested interval between calls
+        #       -- this value is extracted from hf_monitor_config_n_control
+        #       interval_s integer,
+        #       trigger_s integer,
+        #       order_time timestamptz,
+        #       last_started_time timestamptz,
+        #       last_completed_time timestamptz,
+        #       call_counter integer
+        #);
 
-    set batch_lists_len [llength $batch_lists]
-    set dur_sum 0
-    set first_started_time [lindex [lindex $batch_lists 0] 6]
-    if { $debug_p } {
-        ns_log Notice "hf::monitor::do.39: first_started_time '${first_started_time}' batch_lists_len ${batch_lists_len}"
-    }
-    if { $first_started_time eq "" } {
-        if { $batch_lists_len > 0 } {
-            set bi 0
-            # if loop nears cycle_time, quit and let next cycle reprioritize with any new jobs
-            while { $bi < $batch_lists_len && $dur_sum < $cycle_time } {
-                set mon_list [lindex $batch_lists $bi]
-                # set proc_list lindex combo from sched_list
-                lassign $mon_list id proc_name user_id instance_id priority order_clock_s last_started_clock_s last_completed_clock_s last_process_s interval_s dynamic_priority trigger_s
-                # instance_id can vary with each entry
-                
-                set allowed_procs [parameter::get -parameter MonitorProcsAllowed -package_id $instance_id]
-                # added comma and period to "split" to screen external/private references and poorly formatted lists
-                set allowed_procs_list [split $allowed_procs " ,."]
-                set success_p [expr { [lsearch -exact $allowed_procs_list $proc_name] > -1 } ]
-                if { $success_p } {
-                    if { $proc_name ne "" } {
-                        ns_log Notice "hf::monitor::do.54 evaluating id $id"
-                        set nowts [dt_systime -gmt 1]
-                        set start_sec [clock seconds]
-                        # tell the system I am working on it.
-                        set success_p 1
-                        db_dml hf_beat_stack_started {
-                            update hf_beat_stack set started_time =:nowts, last_started_clock_s =:start_sec where id =:id
-                        }
-                        
-                        #ns_log Notice "hf::monitor::do.69: id $id to Eval: '${proc_list}' list len [llength $proc_list]."
-                        # This works in tcl env. should work here:
-                        # get asset attributes
-                          # getasset type
-                          # switch $asset_type ...
-                        # ss { hf_ss_read $id} ...
-                        if {  [catch { set calc_value [eval $proc_name] } this_err_text] } {
-                            ns_log Warning "hf::monitor::do.71: id $id Eval '${proc_list}' errored with ${this_err_text}."
-                            # don't time an error. This provides a way to manually identify errors via sql sort
+        set batch_lists_len [llength $batch_lists]
+        set dur_sum 0
+        set first_started_time [lindex [lindex $batch_lists 0] 6]
+        if { $debug_p } {
+            ns_log Notice "hf::monitor::do.39: first_started_time '${first_started_time}' batch_lists_len ${batch_lists_len}"
+        }
+        if { $first_started_time eq "" } {
+            if { $batch_lists_len > 0 } {
+                set bi 0
+                # if loop nears cycle_time, quit and let next cycle reprioritize with any new jobs
+                while { $bi < $batch_lists_len && $dur_sum < $cycle_time } {
+                    set mon_list [lindex $batch_lists $bi]
+                    # set proc_list lindex combo from sched_list
+                    lassign $mon_list id proc_name user_id instance_id priority order_clock_s last_started_clock_s last_completed_clock_s last_process_s interval_s dynamic_priority trigger_s
+                    # instance_id can vary with each entry
+                    
+                    set allowed_procs [parameter::get -parameter MonitorProcsAllowed -package_id $instance_id]
+                    # added comma and period to "split" to screen external/private references and poorly formatted lists
+                    set allowed_procs_list [split $allowed_procs " ,."]
+                    set success_p [expr { [lsearch -exact $allowed_procs_list $proc_name] > -1 } ]
+                    if { $success_p } {
+                        if { $proc_name ne "" } {
+                            ns_log Notice "hf::monitor::do.54 evaluating id $id"
                             set nowts [dt_systime -gmt 1]
-                            set success_p 0
-                            db_dml hf_beat_stack_write {
-                                update hf_beat_stack set proc_out =:this_err_text, completed_time=:nowts where id = :id 
-                            } 
-                            # inform user of error
-                            set scenario_tid [lindex [lindex $args_lists 0] 0]
-                            hf_log_create $scenario_tid "#hosting-farm.process#" "error" "id ${id} Message: ${this_err_text}" $user_id $instance_id
-                        } else {
-                            set dur_sec [expr { [clock seconds] - $start_sec } ]
-                            # part of while loop so that remaining processes are re-prioritized with any new ones:
-                            set dur_sum [expr { $dur_sum + $dur_sec } ]
-                            set nowts [dt_systime -gmt 1]
+                            set start_sec [clock seconds]
+                            # tell the system I am working on it.
                             set success_p 1
-                            db_dml hf_beat_stack_write {
-                                update hf_beat_stack set proc_out =:calc_value, completed_time=:nowts, process_seconds=:dur_sec where id = :id 
+                            db_dml hf_beat_stack_started {
+                                update hf_beat_stack set started_time =:nowts, last_started_clock_s =:start_sec where id =:id
                             }
-                            ns_log Notice "hf::monitor::do.83: id $id completed in circa ${dur_sec} seconds."
+                            
+                            #ns_log Notice "hf::monitor::do.69: id $id to Eval: '${proc_list}' list len [llength $proc_list]."
+                            # This works in tcl env. should work here:
+                            # get asset attributes
+                            # getasset type
+                            # switch $asset_type ...
+                            # ss { hf_ss_read $id} ...
+                            if {  [catch { set calc_value [eval $proc_name] } this_err_text] } {
+                                ns_log Warning "hf::monitor::do.71: id $id Eval '${proc_list}' errored with ${this_err_text}."
+                                # don't time an error. This provides a way to manually identify errors via sql sort
+                                set nowts [dt_systime -gmt 1]
+                                set success_p 0
+                                db_dml hf_beat_stack_write {
+                                    update hf_beat_stack set proc_out =:this_err_text, completed_time=:nowts where id = :id 
+                                } 
+                                # inform user of error
+                                set scenario_tid [lindex [lindex $args_lists 0] 0]
+                                hf_log_create $scenario_tid "#hosting-farm.process#" "error" "id ${id} Message: ${this_err_text}" $user_id $instance_id
+                            } else {
+                                set dur_sec [expr { [clock seconds] - $start_sec } ]
+                                # part of while loop so that remaining processes are re-prioritized with any new ones:
+                                set dur_sum [expr { $dur_sum + $dur_sec } ]
+                                set nowts [dt_systime -gmt 1]
+                                set success_p 1
+                                db_dml hf_beat_stack_write {
+                                    update hf_beat_stack set proc_out =:calc_value, completed_time=:nowts, process_seconds=:dur_sec where id = :id 
+                                }
+                                ns_log Notice "hf::monitor::do.83: id $id completed in circa ${dur_sec} seconds."
+                            }
+                            # Alert user that job is done?  
+                            # util_user_message doesn't accept user_id instance_id, only session_id
+                            # We don't have session_id available.. and it may have changed or not exist..
+                            # Email?  that would create too many alerts for lots of quick jobs.
+                            # auth::sync::job::* api does this.
+                            # Create another package for user conveniences like active alerts..
+                            # maybe hook into util_user_message after querying users.n_sessions or something..
                         }
-                        # Alert user that job is done?  
-                        # util_user_message doesn't accept user_id instance_id, only session_id
-                        # We don't have session_id available.. and it may have changed or not exist..
-                        # Email?  that would create too many alerts for lots of quick jobs.
-                        # auth::sync::job::* api does this.
-                        # Create another package for user conveniences like active alerts..
-                        # maybe hook into util_user_message after querying users.n_sessions or something..
+                    } else {
+                        ns_log Warning "hf::monitor::do.87: id $id proc_name '${proc_name}' attempted but not allowed. user_id ${user_id} instance_id ${instance_id}"
                     }
-                } else {
-                    ns_log Warning "hf::monitor::do.87: id $id proc_name '${proc_name}' attempted but not allowed. user_id ${user_id} instance_id ${instance_id}"
+                    # next batch index
+                    incr bi
                 }
-                # next batch index
-                incr bi
+            } else {
+                # if do is idle, delete some (limit 100 or so) used args in hf_sched_proc_args. Ids may have more than 1 arg..
+                if { $debug_p } {
+                    ns_log Notice "hf::monitor::do.91: Idle. Entering passive maintenance mode. deleting up to 60 used args, if any."
+                }
+                set success_p 1
+                db_dml hf_sched_proc_args_delete { delete from hf_sched_proc_args 
+                    where stack_id in ( select id from hf_beat_stack where process_seconds is not null order by id limit 60 ) 
+                }
             }
         } else {
-            # if do is idle, delete some (limit 100 or so) used args in hf_sched_proc_args. Ids may have more than 1 arg..
-            if { $debug_p } {
-                ns_log Notice "hf::monitor::do.91: Idle. Entering passive maintenance mode. deleting up to 60 used args, if any."
-            }
+            ns_log Notice "hf::monitor::do.97: Previous hf::monitor::do still processing. Stopping."
+            # the previous hf::monitor::do is still working. Don't clobber. Quit.
             set success_p 1
-            db_dml hf_sched_proc_args_delete { delete from hf_sched_proc_args 
-                where stack_id in ( select id from hf_beat_stack where process_seconds is not null order by id limit 60 ) 
-            }
+        }
+        if { $debug_p || !$success_p } {
+            ns_log Notice "hf::monitor::do.99: returning success_p ${success_p}"
         }
     } else {
-        ns_log Notice "hf::monitor::do.97: Previous hf::monitor::do still processing. Stopping."
-        # the previous hf::monitor::do is still working. Don't clobber. Quit.
-        set success_p 1
-    }
-    if { $debug_p || !$success_p } {
-        ns_log Notice "hf::monitor::do.99: returning success_p ${success_p}"
+        # a previous monitor process is still in progress
+        ns_log Notice "hf::monitor::do.242: previous monitor active_id '${active_id}' processing. Cancelled new batch processing."
     }
     return $success_p
 }
