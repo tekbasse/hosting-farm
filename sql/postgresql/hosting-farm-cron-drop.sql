d59c72d61a0173ecb53ac3fb077c0b0126a8eb14
Benjamin Brink <tekbasse@yahoo.com>
2016-05-05 19:07:31 -0700

    fix index drop errors

--- sql/postgresql/hosting-farm-cron-drop.sql
+++ sql/postgresql/hosting-farm-cron-drop.sql
@@ -15,15 +15,12 @@ DROP TABLE hf_sched_proc_stack;
 
 DROP TABLE hf_sched_params;
 
-DROP index hf_process_log_id_idx;
-DROP index hf_process_log_instance_id_idx;
-DROP index hf_process_log_user_id_idx;
-DROP index hf_process_log_table_tid_idx;
-DROP index hf_process_log_trashed_p_idx;
+
+
 DROP index hf_process_log_viewed_id_idx;
 DROP index hf_process_log_viewed_instance_id_idx;
 DROP index hf_process_log_viewed_user_id_idx;
-DROP index hf_process_log_viewed_table_tid_idx;
+
 
 DROP TABLE hf_process_log_viewed;
 
