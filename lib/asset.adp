697654d44fadaef7fa634c2630de4c04e7ff723c
tekbasse <tekbasse@yahoo.com>
2016-06-14 18:26:06 -0700

    first draft asset view

--- lib/asset.adp
+++ lib/asset.adp
@@ -1,1 +1,34 @@
-@content;noquote@
+
+
+<img src="/resources/icons/@asset_type_id@.png" title="@asset_title;noquote@" alt="@asset_title;noquote@">
+
+<div style="background: url (/resources/icons/@asset_type_id@-background.png); background-size: contain;">
+
+<h3>@label@</h3>
+<p>@title@</p>
+
+<ul>
+  <if @detail_p@>
+    <li>
+      qal_product_id @qal_product_id@
+    </li><li>
+      publish_p      @publish_p@
+    </li><li>
+      monitor_p      @monitor_p@
+    </li><li>
+      trashed_p      @trashed_p@
+    </li>
+  </if>
+  
+  <if @tech_p@>
+    <li>
+      op_status   @op_status@
+    </li><li>
+      template_p  @template_p@
+    </li><li>
+      templated_p @templated_p@
+    </li><li>
+      triage_priority @triage_priority@
+    </li>
+  </if>
+</ul>
--- lib/asset.tcl
+++ lib/asset.tcl
@@ -1,15 +1,81 @@
 # hosting-farm/lib/asset.tcl
-# show a list of hf asset
+# show an hf asset record
 #
 
 # requires:
 # @param array with elements of hf_asset_keys
 # optional:
+#  detail_p      If detail_p is 1, flags to show record detail
+#  tech_p        If is 1, flags to show technical info (for admins)
+
+
+# provides variables of each element:
+# from hf_asset_keys:
 #
+#  asset_id
+#  
+#  label
+#  
+#  name
+#  
+#  asset_type_id
+#   and related:
+    asset_label asset_title asset_description
+#  
+#  trashed_p
+#  
+#  trashed_by
+#  
+#  template_p
+#  
+#  templated_p
+#  
+#  publish_p
+#  
+#  monitor_p
+#  
+#  popularity
+#  
+#  triage_priority
+#  
+#  op_status
+#  
+#  qal_product_id
+#  
+#  qal_customer_id
+#  
+#  instance_id
+#  
+#  user_id
+#  
+#  last_modified
+#  
+#  created
+#  
+#  flags
+#  
+#  template_id
+#  
+#  f_id
+
+# and some related data
 
 # to pass array via include, see: /doc/acs-templating/tagref/include
 # ie: &asset_arr="calling_page_arr_name"
 #
-#
-# 
+
+if { ![info exists detail_p] } {
+    set detail_p 0
+}
+if { ![info exists tech_p] } {
+    set tech_p 0
+}
+if { $tech_p } {
+    set detail_p 1
+}
+
+
 qf_array_to_vars asset_arr [hf_asset_keys ]
+
+set asset_type_list [lindex [hf_asset_type_read $asset_type_id $instance_id] 0]
+qf_lists_to_vars $asset_type_list [list asset_label asset_title asset_description]
