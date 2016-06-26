4b7106098cfaf53d1dac057b1bf3db1a60fcc2ba
Benjamin Brink <tekbasse@yahoo.com>
2016-06-26 00:25:56 -0400

    more code

--- www/admin/index.tcl
+++ www/admin/index.tcl
@@ -1,5 +1,5 @@
 set title "#acs-subsite.Administration#"
-set context [list $title]
+set context [list ]
 
 set contents "hf_domain_example [hf_domain_example]"
 
---
+++ www/permissions-table.tcl
@@ -0,0 +1,7 @@
+set title "#acs-subsite.Permissions#"
+set context [list $title]
+
+set permissions_table_html ""
+
+
+# table showing 
