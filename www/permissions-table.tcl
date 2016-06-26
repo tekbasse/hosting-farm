ca8ae937ebb282ac6c74f635cfe93cf889aa0d5c
Benjamin Brink <tekbasse@yahoo.com>
2016-06-26 00:48:44 -0400

    added permissions display notes

--- www/permissions-table.tcl
+++ www/permissions-table.tcl
@@ -4,4 +4,21 @@ set context [list $title]
 set permissions_table_html ""
 
 
-# table showing 
+
+
+
+# if query_customer_id exists 
+# table showing roles assigned for each user of customer_id
+
+# if query_user_id exists
+# roles for user for each customer_id
+# 
+
+# if admin_p:
+
+# table showing privileges 
+#
+# role vs property_label, 
+# otherwise 
+# if query_user_id exists limit roles to only those of user_id
+
