c16d18d730b0d29e253933365ec9e1abd17a24fe
Benjamin Brink <tekbasse@yahoo.com>
2016-06-27 22:04:11 -0400

    fix paren mismatch and query

--- tcl/hosting-farm-attr-view-procs.tcl
+++ tcl/hosting-farm-attr-view-procs.tcl
@@ -276,7 +276,7 @@ ad_proc -private hf_os_read {
     upvar 1 instance_id instance_id
     upvar 1 user_id user_id
     set os_id_filtered_list [hf_list_filter_by_natural_number $os_id_list]
-    set rows_list [db_list_of_lists hf_os_detail_get "select [hf_os_keys ","] from hf_operating_systems where instance_id =:instance_id and os_id in ([template::util::tcl_to_sql_list $os_id_filtered_list]"]
+    set rows_list [db_list_of_lists hf_os_detail_get "select [hf_os_keys ","] from hf_operating_systems where instance_id =:instance_id and os_id in ([template::util::tcl_to_sql_list $os_id_filtered_list]) and time_trashed=NULL"]
     return $return_list
 }
 
