5d5e1558a3ff79ad0699f3a57cb1a128f67a65f5
Benjamin Brink <tekbasse@yahoo.com>
2016-06-28 13:37:50 -0400

    code toward testing

--- tcl/test/hosting-farm-test-procs.tcl
+++ tcl/test/hosting-farm-test-procs.tcl
@@ -3,17 +3,20 @@ ad_library {
     @creation-date 2015-03-19
 }
 
-aa_register_case permissions_check {
+aa_register_case -cats {api smoke} permissions_check {
     Test hf_permissions_p proc for all cases
 } {
     aa_run_with_teardown \
         -rollback \
         -test_code {
-
-ns_log Notice "aa_register_case.13: Begin test permissions_check"
-aa_true "Test for .." $passed_p
-aa_equals "Test for .." $test_value $expected_value
-
-}
-
+            
+            ns_log Notice "aa_register_case.13: Begin test permissions_check"
+            # Use default permissions provided by tcl/hosting-farm-init.tcl
+            set instance_id ""
+            
+            
+            #aa_true "Test for .." $passed_p
+            #aa_equals "Test for .." $test_value $expected_value
+            
+        }
 }
