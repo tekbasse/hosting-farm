d89d9999ff0d4058099111cf5c9e12689435cd64
Benjamin Brink <tekbasse@yahoo.com>
2016-07-27 18:08:23 -0400

    renamed file for package manager compliance

--- tcl/test/hosting-farm-test-perms-procs.tcl
+++ tcl/test/hosting-farm-test-perms-procs.tcl
@@ -3,7 +3,7 @@ ad_library {
     @creation-date 2015-03-19
 }
 
-aa_register_case -cats {db smoke} asset_attr_permutation_check {
+aa_register_case -cats {db smoke} asset_permutations_check {
     tests permutations of each asset and attribute via api
 } {
     aa_run_with_teardown \
